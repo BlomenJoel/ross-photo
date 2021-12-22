98a8ce63fb8aac00b0ec276665be65947f07b3a0
joel@queenslab <joel@queenslab.se>
2021-12-22 16:40:56 +0100

    remove stuff

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,6 @@
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
 </script>
 
 <svelte:head>
@@ -11,22 +10,15 @@
 </svelte:head>
 
 <section>
+	<div class="welcome">
+		<picture>
+			<source srcset="svelte-welcome.webp" type="image/webp" />
+			<img src="svelte-welcome.png" alt="Welcome" />
+		</picture>
+	</div>
 	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
+		to Ross's gallery
 	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
 </section>
 
 <style>
